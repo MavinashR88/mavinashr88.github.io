56e868564cfd4bd9550b5dfa301b5d21c0178f55
mavinashr88 <manneavinashreddy95@gmail.com>
2025-05-05 19:22:31 -0500

    Update vite config with correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: './',
+  base: '/portfolio-/', // Change from './' to '/portfolio-/'
   build: {
     assetsDir: 'assets',
     rollupOptions: {
